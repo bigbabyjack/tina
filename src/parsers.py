7e1483fef5bf13fdf50bb26673acfbcf43d662cb
Jack Conner <1jackconner@gmail.com>
2024-04-28 16:36:30 -0700

    add docs

--- src/parsers.py
+++ src/parsers.py
@@ -10,6 +10,24 @@ class AbstractQueryParser(ABC):
 
 
 class QueryParser(AbstractQueryParser):
+    """
+    Documentation for QueryParser:
+    The QueryParser class is an abstract class that defines the interface for parsing user queries.
+    The parse() method should be implemented by subclasses to parse the input query and return a UserInput object.
+
+    Example:
+    class MyQueryParser(QueryParser):
+        def parse(self, query: str) -> UserInput:
+            return UserInput(query=query)
+
+    my_query_parser = MyQueryParser()
+    user_input = my_query_parser.parse("hello world")
+    print(user_input.query)  # Output: "hello world"
+    """
+
+    def __init__(self):
+        pass
+
     def parse(self, query: str) -> UserInput:
         return UserInput(query=query)
 
@@ -21,5 +39,23 @@ class AbstractResponseParser(ABC):
 
 
 class ResponseParser(AbstractResponseParser):
+    """
+    Documentation for ResponseParser:
+    The ResponseParser class is an abstract class that defines the interface for parsing query responses.
+    The parse() method should be implemented by subclasses to parse the input query and return a QueryResponse object.
+
+    Example:
+    class MyResponseParser(ResponseParser):
+        def parse(self, response: str) -> QueryResponse:
+            return QueryResponse(response=response)
+
+    my_response_parser = MyResponseParser()
+    query_response = my_response_parser.parse("hello world")
+    print(query_response.response)  # Output: "hello world"
+    """
+
+    def __init__(self):
+        pass
+
     def parse(self, response: str) -> QueryResponse:
         return QueryResponse(response=response)
