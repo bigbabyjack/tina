08fa6debf389a467abc1bccef1f3e5ba49b42769
Jack Conner <1jackconner@gmail.com>
2024-04-28 20:46:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,26 +4,6 @@ from src.datastructures import ServiceContext
 from src.llms import LanguageModelNames
 from src.orchestrator import LLMOrchestrator
 
-# # Set the URL for the LLaMA model's chat API endpoint
-# url = "http://127.0.0.1:11434/api/generate"
-#
-# # Set the headers for the request
-#
-# # Set the data for the chat API request
-data = {
-    "model": "llama3:8b",
-    "prompt": "I am making a CLI tool that uses Llama3 as an agent for carrying out different actions based on the user text input.",
-    "stream": False,
-}
-#
-# # Send a POST request to the LLaMA model's chat API endpoint
-# response = requests.post(url, json=data)
-# response.raise_for_status()
-# response = response.json()
-#
-# message_response = response["response"]
-# print(message_response)
-
 
 def print_process(service_context: ServiceContext) -> None:
     print(
