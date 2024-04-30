ccfb9cb6ee97656dfabb2d633bfa4f701e6886ce
Jack Conner <jack@jc.lan>
2024-04-29 20:48:51 -0700

    docs

--- src/executors.py
+++ src/executors.py
@@ -11,10 +11,24 @@ class AbstractExecutor(ABC):
 
 
 class CommandExecutor(AbstractExecutor):
+    """
+    Documentation for CommandExecutor:
+    The CommandExecutor class is an abstract class that defines the interface for executing commands.
+
+    Example:
+    class MyCommandExecutor(CommandExecutor):
+        def execute(self, context: ServiceContext) -> ServiceContext:
+            return context
+
+    my_command_executor = MyCommandExecutor()
+    service_context = my_command_executor.execute(service_context)
+    """
+
     def execute(self, context: ServiceContext) -> ServiceContext:
         if context.input_arguments["search"]:
             query = context.input_query
             self.open_google_search(query)
+        context.response = f"googling: {context.input_query}"
         return context
 
     def open_google_search(self, query):
