d320e2864363bda569dad516390a9b5155b69469
Jack Conner <1jackconner@gmail.com>
2024-04-30 20:26:29 -0700

    implement logging

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from src.parsers import InputArgumentParser
 def main():
     setup_logging()
     logging.debug("Starting the program...")
+    logging.debug("Creating the service context...")
 
     context = ServiceContext(
         logger=logging.getLogger(__name__),
@@ -22,13 +23,20 @@ def main():
         parsed_response="",
         orchestration_plan=[],
     )
+    context.logger.debug("Parsing the user input...")
     input_parser = InputArgumentParser()
     context = input_parser.parse(context)
+    context.logger.debug(f"Parsed query: {context.parsed_query}")
+    context.logger.debug("Starting orchestration planning...")
     context = OrchestratorPlanner().plan(context)
+    context.logger.debug(f"Orchestration plan: {context.orchestration_plan}")
+    context.logger.debug("Starting orchestration...")
     context = LLMOrchestrator(
         service_context=context,
         model_name=LanguageModelNames.LLAMA3_8B,
     ).orchestrate()
+    context.logger.debug("Orchestration completed")
+    context.logger.debug(f"Final service context: {context}")
 
     print(f"{context.parsed_response}")
 
--- src/logger.py
+++ src/logger.py
@@ -1,13 +1,33 @@
 import time
 import logging
 from functools import wraps
+import os
 
 
-def setup_logging():
+def setup_logging(logging_level=logging.DEBUG, log_to_console=False, log_to_file=True):
+    # Define the log format
+    log_format = "%(asctime)s - %(module)s - %(funcName)s - %(levelname)s - %(message)s"
+
+    # Set up basic configuration
     logging.basicConfig(
-        level=logging.DEBUG,
-        format="%(asctime)s - %(module)s - %(funcName)s - %(levelname)s - %(message)s",
-    )
+        level=logging_level,
+        format=log_format,
+        handlers=[logging.FileHandler("application.log")],
+    )  # Default handler for file output
+
+    # Conditionally add console output
+    if log_to_console:
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging_level)
+        console_handler.setFormatter(logging.Formatter(log_format))
+        logging.getLogger().addHandler(console_handler)
+
+    # Conditionally add console output
+    if log_to_file:
+        file_handler = logging.FileHandler("./logs/application.log", mode="w")
+        file_handler.setLevel(logging_level)
+        file_handler.setFormatter(logging.Formatter(log_format))
+        logging.getLogger().addHandler(file_handler)
     # Silence noisy libraries
     logging.getLogger("urllib3").setLevel(logging.WARNING)
 
