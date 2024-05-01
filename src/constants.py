3ecfb5bbd316029c7ded51e5ad03653c322a4d6c
Jack Conner <1jackconner@gmail.com>
2024-04-30 17:54:17 -0700

    add constants file

---
+++ src/constants.py
@@ -0,0 +1,9 @@
+from enum import StrEnum
+
+
+class OrchestrationSteps(StrEnum):
+    INPUT_ARGUMENT_PARSING = "input_argument_parsing"
+    QUERY_PARSING = "query_parsing"
+    LLM_INVOCATION = "llm_invocation"
+    ACTION_EXECUTION = "action_execution"
+    RESPONSE_PARSING = "response_parsing"
